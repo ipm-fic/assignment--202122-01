173bd0dfc1d42149d1b4312596daf74469230e9e
David Cabrero Souto <david.cabrero@madsgroup.org>
2021-09-03 13:46:15 +0200

    ADDED: enunciado sin info del servidor

---
+++ enunciado/.eleventy.js
@@ -0,0 +1,12 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("vendor");
+  eleventyConfig.addPassthroughCopy("img");
+
+  eleventyConfig.addPlugin(syntaxHighlight);
+};
+
+
